e8bed2fc216392feaac9fcb733df23f03b602f3a
ROJAS Juan Ana Karina <rojasjuananakarina@gmail.com>
2021-12-15 13:30:25 -0600

    primera parte del trabajo

---
+++ src/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
+
+//initializations
+const app = express();
+
+//settings
+app.set('port', process.env.PORT || 4000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs({
+    defaultLayaout: 'main', 
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+
+//middlewares
+app.use(express.urlencoded({extended: false}));
+
+//global variables
+
+//routes
+app.get('/', (req, res) => {
+    res.render('index')
+});
+//static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+module.exports = app;
